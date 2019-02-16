5c5dc64b7de18c696dc29a6304d2446c9fdc83f8
chenhuiChang <j4demacia@gmail.com>
2019-02-16 15:23:30 +0800

    first testcase files

---
+++ src/Accounting.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexchang
+ * Date: 2019-02-16
+ * Time: 15:20
+ */
+
+namespace App;
+
+
+class Accounting
+{
+
+
+}
---
+++ tests/AccountingTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexchang
+ * Date: 2019-02-16
+ * Time: 15:20
+ */
+
+namespace Tests;
+
+use App\Accounting;
+use PHPUnit\Framework\TestCase;
+
+class AccountingTest extends TestCase
+{
+}
