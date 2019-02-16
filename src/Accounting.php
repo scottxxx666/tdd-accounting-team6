ac4e71b7565664887f30dcf7e5444bb3d93ea5c3
Scott Kao <scottxxx666@gmail.com>
2019-02-17 01:36:48 +0800

    Pass not between budge

--- src/Accounting.php
+++ src/Accounting.php
@@ -43,27 +43,16 @@ class Accounting
         $totalBudget = 0;
         foreach ($this->getBudgetList() as $budget) {
             $budgetYearMonth = $this->getBudgetYearMonth($budget);
-            if (!$this->isCrossMonth()) {
-                return $budget->getAmount() * ($this->end->diffInDays($this->start) + 1) / $budgetYearMonth->daysInMonth;
-            } else {
-//                echo '~' . $budgetYearMonth->startOfMonth()->toDateString() . "\n";
-                $startDay = $budgetYearMonth->startOfMonth()->gt($this->start) ? $budgetYearMonth->startOfMonth()->copy() : $this->start;
-                $endDay = $budgetYearMonth->endOfMonth()->gt($this->end) ? $this->end : $budgetYearMonth->endOfMonth()->copy();
-//                echo 'start: ' . $startDay->format('Y-m-d') . "\n";
-//                echo 'end: ' . $endDay->format('Y-m-d') . "\n";
-                $sumDays = $endDay->diffInDays($startDay) + 1;
-//                echo '-' . $budgetYearMonth->startOfMonth()->toDateString() . "\n";
-//                echo $budgetYearMonth->format('Y-m') . "\n";
-                echo $sumDays . "\n";
-//
-//                echo 'start: ' . $startDay->format('Y-m-d') . "\n";
 
-                $totalBudget += $budget->getAmount() * ($sumDays /
-                        $budgetYearMonth->daysInMonth
-                    );
+            if ($budgetYearMonth->startOfMonth()->gt($end) || $budgetYearMonth->endOfMonth()->lt($start)) {
+                continue;
             }
-
+            $startDay = $budgetYearMonth->startOfMonth()->gt($this->start) ? $budgetYearMonth->startOfMonth()->copy() : $this->start;
+            $endDay = $budgetYearMonth->endOfMonth()->gt($this->end) ? $this->end : $budgetYearMonth->endOfMonth()->copy();
+            $sumDays = $endDay->diffInDays($startDay) + 1;
+            $totalBudget += $budget->getAmount() * $sumDays / $budgetYearMonth->daysInMonth;
         }
+
         return $totalBudget;
     }
 
@@ -91,22 +80,5 @@ class Accounting
     {
         $budgetYearMonth = Carbon::create(substr($budget->getYearMonth(), 0, 4), substr($budget->getYearMonth(), 4, 2));
         return $budgetYearMonth;
-    }
-
-    /**
-     * @return mixed
-     */
-    private function isCrossMonth()
-    {
-        return !$this->start->isSameMonth($this->end);
-    }
-
-    /**
-     * @param $budgetYearMonth
-     * @return mixed
-     */
-    private function inRange($budgetYearMonth)
-    {
-        return $budgetYearMonth->between($this->start, $this->end);
     }
 }
